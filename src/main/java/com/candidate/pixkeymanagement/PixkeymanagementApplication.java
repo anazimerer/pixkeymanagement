58748f6754c5b29311029f229fdade0a4c4b892d
anazimerer <ana.zimerer@gmail.com>
2024-09-24 22:32:06 -0300

    Initial configurations

---
+++ src/main/java/com/candidate/pixkeymanagement/PixkeymanagementApplication.java
@@ -0,0 +1,14 @@
+package com.candidate.pixkeymanagement;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixkeymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixkeymanagementApplication.class, args);
+	}
+
+}
